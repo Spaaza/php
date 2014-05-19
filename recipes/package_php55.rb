34b468a0bcf194d4ef29ef85c946f493ce8f766d
Sam Critchley <sam.critchley@spaaza.com>
2014-05-19 14:09:52 +0000

    Removed systemd package

--- recipes/package_php55.rb
+++ recipes/package_php55.rb
@@ -23,14 +23,6 @@ include_recipe "apt"
 if node["platform_family"] == "debian"
   case node["lsb"]["codename"]
   when "precise", "quantal", "raring"
-    apt_repository "ondrej-systemd" do
-      uri "http://ppa.launchpad.net/ondrej/systemd/ubuntu"
-      distribution node["lsb"]["codename"]
-      components ["main"]
-      deb_src true
-      keyserver "keyserver.ubuntu.com"
-      key "E5267A6C"
-    end
     apt_repository "ondrej-php55" do
       uri "http://ppa.launchpad.net/ondrej/php5/ubuntu"
       distribution node["lsb"]["codename"]
